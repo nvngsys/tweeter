f30d4772262b1e815639e9bdc7bff23a9484d2dc
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-15 16:01:16 +0000

    upload index.html and app.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,45 @@
-// console.log(`testing composer-char-counter.js`);
-
 $(document).ready(function () {
-    console.log(`testing composer-char-counter.js`);
+    //console.log(`testing composer-char-counter.js`);
 
-
-    $("textarea[name=text]").on('keydown', function () {
+    // $("textarea").on("keydown keyup keypress input change", function (event) {
+    //   console.log(event.type, "textarea value:", $(this).val());
+    // });
+    $("textarea[name=text]").on('input', function () {
         //console.log($(this).val()); //The this keyword is a reference to the button
         let count = $(this).val().length;
+        const tweetMax = 140;
+        const errorClass = 'error';
         console.log(count);
-        if (count <= 5) {
-            $(".counter").html(140 - count);
+        const $counter = $(this).siblings('.counter');
+        if (count <= tweetMax) {
+            $counter.removeClass(errorClass);
         } else {
-            // make the value red
-            $('.counter').css('color', 'red');
+            // change value to red when reaches 140 characters
+            $counter.addClass(errorClass);
         }
+        $counter.html(tweetMax - count);
     });
+    // $("article").hover(function () {
+    //     //console.log("test hover");
+    //     const onHoverClass = 'onHover';
+    //     //$(this).css("background-color", "yellow");  //this does work
+    //     //$(this).addClass("onHover"); // this is not working
+    //     $(this).find("h2").stop().fadeTo('slow',1);
+    // }, function () {
+    //     //$(this).css("background-color", "lightgray");
+    //     $(this).find("h2").stop().fadeTo('slow',1);
+    // });
+
+    // $("article").mouseover(function(){
+    //     $("article").css("background-color", "yellow");
+    //   });
+    //   $("article").mouseout(function(){
+    //     $("article").css("background-color", "lightgray");
+    //   });
+
+    $( "article" ).hover(function() {
+        $( this ).fadeOut( 100 );
+        $( this ).fadeIn( 500 );
+      });
 
 })
