65dede4f143a325c027a69169f916506330c74ec
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-17 03:47:53 +0000

    remove commented code and logging from app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,12 +10,8 @@
 $(document).ready(function () {
 
     function renderTweets(tweets) {
-        // loops through tweets
-        // calls createTweetElement for each tweet
-        // takes return value and appends it to the tweets container
         let $tweet = {};
         tweets.forEach(item => {
-            //console.log(item); // for each element/object in array pass to createTweetElement()
             $tweet = createTweetElement(item);
             $('.tweet-container').append($tweet);
         });
@@ -28,14 +24,12 @@ $(document).ready(function () {
         let daysAgo = tweet['created_at'];
         let handle = tweet['user']['handle'];
         let imageSource = tweet['user']['avatars']['small'];
+
+        // TO DO ********>
         // ADD THE OTHER IMAGES AT FONTAWESOME - UPDATE APPEND TO ADD TO FOOTER
         // YOU MAY NEED TO ADD TWO ELEMENTS TO FOOTER TO DISPLAY TIME ON LEFT
         // AND FLAGS ETC ON RIGHT
         //let imageFlag = '<i class="fas fa-flag"></i>;
-
-        //console.log(imageSource);
-        // let tweets = $('<article>').addClass('tweet');
-        // let test = $('<header>');
 
         let a = $('<article>').addClass("existing-tweet");
         let hdr = $('<header>');
@@ -43,30 +37,19 @@ $(document).ready(function () {
         div.attr('id', 'headerNames');
         let img = $('<img>');
         let src = $('<src>');
-        // let footer = $('<footer>');
-        //build header
-
-        //$('<img src="/images/bird.png">').appendTo(hdr);  //worked
-        //$('`<img src=${imageSource}>`').appendTo(hdr);   // trying to add source from data
-        //$('.existing-tweet img').attr('src', imageSource);
         img.attr('src', imageSource);
         img.appendTo(hdr);
         div.appendTo(hdr);
         $('<h2>').text(hdrTxt).appendTo(div);
-        // <span> class="handle">user address</span>  adding May 16
         $('<span>').addClass("handle").text(handle).appendTo(div);
-        //
         hdr.appendTo(a);
         $('<p>').text(content).appendTo(a);
         $('<footer>').text(daysAgo).appendTo(a);
 
-
-        console.log(a);
         return a;
     }
     function loadTweets() {
         $.get("/tweets/", function (data) {
-            //alert( "Load was performed." );
             renderTweets(data);
         });
     }
@@ -78,8 +61,6 @@ $(document).ready(function () {
     $("form").submit(function (event) {
         event.preventDefault();
         $('.tweet-container').empty();
-        //alert("Testing - Handler click for .click() called...");
-        /** send data to server below - these three values are required */
         let maxLen = 140;
         let submitTrue = true;
         let $form = $(this);
@@ -88,36 +69,29 @@ $(document).ready(function () {
 
         /** perform text validation before sending */
         if (term === null || term === "") {
-            //alert(`Tweet cannot be empty`);
-            // move this to the element and call .unhide
             submitTrue = false;
             $('#tweetErr').text("Your tweet is empty!");
             $('#tweetErr').removeClass('hide');
         }
 
         if (term.length > maxLen) {
-            //alert(`Tweet cannot be more than 140 characters`);
             submitTrue = false;
-            console.log(`length error`)
             $('#tweetErr').text(`Tweet exceeds ${maxLen} characters!`);
             $('#tweetErr').removeClass('hide');
         }
 
-        // console.log(term);
-        // console.log(url);
         if (submitTrue) {
             var posting = $.post(url, { text: term });
-            loadTweets();
+            //loadTweets();
             $form.find("textarea[name=text]").val('');
         } else {
             //clear the input field
             $form.find("textarea[name=text]").val('');
         }
-
+        loadTweets();
     });
 
     $("#nav-bar [type=button]").click(function () {
-        //alert("nav bar button click.");
         $(".new-tweet").slideDown().find("#txt1").focus();
     });
 
